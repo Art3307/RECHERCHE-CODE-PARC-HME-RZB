9998178bfa6ff9422995f5d89ab26c6a2383b03b
Art3307 <arthurcahn@gmail.com>
2026-02-26 10:54:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 
 st.set_page_config(
-    page_title="Parc HME ↔ RZB https://upload.wikimedia.org/wikipedia/commons/c/ce/Logo_Razel-Bec.jpg",
+    page_title="Parc 🔴 HME ↔ RZB 🔷",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
